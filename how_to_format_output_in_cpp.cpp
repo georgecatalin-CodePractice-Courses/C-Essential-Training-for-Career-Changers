f92a5d20101f0f78175ce529d7d542a499b3e9a6
George Calin <george.calin@gmail.com>
2020-04-21 13:57:58 +0300

    How to format output in C++.

---
+++ how_to_format_output_in_cpp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout<<"AL"<<setw(15)<<left<<"\tAlabama"<<fixed<<setprecision(2)<<4.00<<"%"<<endl;
+    cout<<"AZ"<<setw(15)<<left<<"\tArizona"<<fixed<<setprecision(2)<<5.60<<"%"<<endl;
+    cout<<"AR"<<setw(15)<<left<<"\tArkansas"<<fixed<<setprecision(2)<<6.50<<"%"<<endl;
+    cout<<"CA"<<setw(15)<<left<<"\tCalifornia"<<fixed<<setprecision(2)<<6.00<<"%"<<endl;
+    cout<<"CO"<<setw(15)<<left<<"\tColorado"<<fixed<<setprecision(2)<<2.90<<"%"<<endl;
+
+    return 0;
+
+}
