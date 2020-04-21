39c8f967b7c1e69281054bfe530db3eab9953c00
George Calin <george.calin@gmail.com>
2020-04-21 11:50:53 +0300

    Create assignment expressions.

---
+++ create_assignment_expressions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a=5, b=7, c=11;
+    double average=(a+b+c)/3.0;
+
+    cout<<average<<endl;
+
+    int r;
+    r=c%2;
+    cout<<"r:"<<r<<endl;
+
+    return 0;
+}
